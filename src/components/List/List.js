382acab8eb8b386f2ed356695985d659ff12dc29
Glenn Law <glennlaw@me.com>
2020-11-25 13:21:11 -0500

    Todo app react exercise project

---
+++ src/components/List/List.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Form from '../Form/Form';
+import Todo from '../Todo/Todo';
+
+function List() {
+  const [todos, setTodo] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodo(newTodos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodo((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodo(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodo(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>What are the task?</h1>
+      <Form onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
+}
+
+export default List;
